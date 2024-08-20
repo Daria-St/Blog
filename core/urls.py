b8da7a9ee6e41e7bb02614ba710cb3fef51d036e
Daria-St <ph.steklova@gmail.com>
2024-08-20 09:07:42 +0500

    Страница добавления заметок

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import main, posts
+from .views import main,  post_add, post_add_submit
 
 urlpatterns = [
-    path('', main),
-    # path('posts', posts),
+    path('', main, name='main'),
+    path('main/add', post_add),
+    path('main/add_submit', post_add_submit, name='post_add_submit'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 def main(request):
@@ -8,3 +8,16 @@ def main(request):
 def posts(request):
     posts = Post.objects.all()
     return render(request, '', {"posts": posts})
+
+def post_add(request):
+
+    return render(request, 'post_add.html')
+
+def post_add_submit(request):
+    title = request.POST.get('title')
+    text = request.POST.get('text')
+
+    Post.objects.create(title=title, text=text)
+
+    print(title, text)
+    return redirect('main')
